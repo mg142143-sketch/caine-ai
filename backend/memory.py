31bd69e92cae7625a993794e121da8355b571493
mg142143-sketch <mg142143@stu.hemetusd.org>
2026-07-13 04:19:30 -0700

    Update memory.py

--- backend/memory.py
+++ backend/memory.py
@@ -1,1 +1,28 @@
+from prompt import SYSTEM_PROMPT
 
+class Memory:
+
+    def __init__(self):
+        self.messages = [
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT
+            }
+        ]
+
+    def add_user(self, text):
+        self.messages.append({
+            "role": "user",
+            "content": text
+        })
+
+    def add_ai(self, text):
+        self.messages.append({
+            "role": "assistant",
+            "content": text
+        })
+
+    def history(self):
+        return self.messages
+
+memory = Memory()
